17dd194f0488493e9a9962b54a712c35c28a3642
pvcresin <pvcresin0730@gmail.com>
2019-05-21 16:35:05 +0900

    Delete unused example config.schemas.js

--- examples/config/config.models.js
+++
@@ -1,9 +0,0 @@
-function snakeToCamel(str) {
-  return str.replace(/_./g, (s) => s.charAt(1).toUpperCase());
-}
-
-module.exports = {
-  modelsDir: './examples/tmp/models',
-  attributeConverter: snakeToCamel,
-  useFlow: false,
-};
