0b22af9a157eaecf88ac0286a3fbbd1893e8fb38
Shuhei Aoyama <aoyama@sansan.com>
2019-06-08 23:23:27 +0900

    Reduce unnecessary props

--- src/tools/js_spec_generator.js
+++ src/tools/js_spec_generator.js
@@ -1,10 +1,14 @@
 const path = require('path');
-const { writeFilePromise, readTemplates, render } = require('./utils');
+const { writeFilePromise, readTemplates, render, walkSchema } = require('./utils');
+
+const UNNECESSARY_PROPS = [
+  '__$ref__', 'description', 'info',
+  'x-model-name', 'x-id-attribute', 'x-attribute-as', 'x-enum-key-attributes',
+];
 
 class JsSpecGenerator {
-  constructor({outputPath = '', templatePath, spec, extension = 'js'}) {
+  constructor({outputPath = '', templatePath, extension = 'js'}) {
     this.outputPath = outputPath;
-    this.spec = spec;
     this.templatePath = templatePath;
     this.templates = readTemplates(['spec', 'head'], this.templatePath);
     const {dir, name} = path.parse(this.outputPath);
@@ -13,13 +17,20 @@ class JsSpecGenerator {
     this.write = this.write.bind(this);
   }
 
-  write(spec = this.spec) {
+  write(spec) {
+    this.reduceUnnecessaryProps(spec);
     const text = render(this.templates.spec, {
       spec: JSON.stringify(spec, null, 2),
     }, {
       head: this.templates.head,
     });
     return writeFilePromise(path.join(this.outputDir, this.outputFileName), text);
+  }
+
+  reduceUnnecessaryProps(spec) {
+    walkSchema(spec, (obj) => {
+      UNNECESSARY_PROPS.forEach((key) => delete obj[key]);
+    });
   }
 }
 
