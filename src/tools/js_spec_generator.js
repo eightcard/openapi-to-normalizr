5d2ed0a378eabf3b5315de4d729143d087ad9bc2
Shuhei Aoyama <aoyama@sansan.com>
2019-06-09 02:27:38 +0900

    Add reducer for unused components - Update snapshot

--- src/tools/js_spec_generator.js
+++ src/tools/js_spec_generator.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const _ = require('lodash');
 const { writeFilePromise, readTemplates, render, walkSchema } = require('./utils');
 
 const UNNECESSARY_PROPS = [
@@ -19,6 +20,7 @@ class JsSpecGenerator {
 
   write(spec) {
     this.reduceUnnecessaryProps(spec);
+    this.reduceUnusedComponents(spec);
     const text = render(this.templates.spec, {
       spec: JSON.stringify(spec, null, 2),
     }, {
@@ -31,6 +33,38 @@ class JsSpecGenerator {
     walkSchema(spec, (obj) => {
       UNNECESSARY_PROPS.forEach((key) => delete obj[key]);
     });
+  }
+
+  reduceUnusedComponents(spec) {
+    const useRefs = {};
+
+    // pathから利用しているrefを取得
+    walkSchema(spec.paths, (obj) => {
+      if (obj.$ref) useRefs[obj.$ref] = true;
+    });
+    // pathで利用しているrefから芋づる式に利用しているrefを取得
+    _.each(useRefs, (_bool, ref) => checkRef(ref));
+
+    // ↑で取得した以外のcomponents情報を削除
+    _.each(spec.components, (schemas, key) => {
+      _.each(schemas, (_obj, name) => {
+        const p = `#/components/${key}/${name}`;
+        if (!useRefs[p]) delete schemas[name];
+      });
+    });
+
+    function checkRef(ref) {
+      const [, components, group, name] = ref.split('/');
+      walkSchema(spec[components][group][name], (obj) => {
+        if (obj.$ref) {
+          // eslint-disable-next-line no-unused-vars
+          const [hash, components, group, name, ...rest] = obj.$ref.split('/');
+          const ref = [hash, components, group, name].join('/');
+          useRefs[ref] = true;
+          checkRef(ref);
+        }
+      });
+    }
   }
 }
 
