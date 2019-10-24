8a364da185b4f7becb2e69ac1d715d32a61bc326
pvcresin <pvcresin0730@gmail.com>
2019-10-24 20:21:25 +0900

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,19 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 2019,
+    project: './tsconfig.json',
   },
   globals: {
     jest: true,
