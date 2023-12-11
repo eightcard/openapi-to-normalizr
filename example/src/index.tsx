13353023ca8e664b7f061f6e2a20e1b12209b29e
pvcresin <pvcresin0730@gmail.com>
2023-12-11 19:45:38 +0900

    Migrate example from Create React App to Vite

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+const rootNode = document.getElementById('root');
+
+if (rootNode) {
+  ReactDOM.createRoot(rootNode).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+}
